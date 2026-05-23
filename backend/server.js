231c85ca03a3e929968bcdd95b5b713ca7f36f8c
Prasanth <prasanths1204@gmail.com>
2026-05-23 09:27:34 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://careermind-eight.vercel.app']
+  origin: '*'
 }))
 app.use(express.json())
 
